4508f66dd55b712978613bbc8205f1c5d79408ad
Denys Samoilov <133922338+denys-samoilov@users.noreply.github.com>
2026-09-19 22:09:13 +0300

    feat: implement generic Library class and StorageService for local state
    - Implemented a generic Library class to manage collection
    - Added add, remove, find, count, and getAll methods to the Library repository
    - Created StorageService to encapsulate LocalStorage logic

--- src/services/Library.ts
+++ src/services/Library.ts
@@ -1,5 +1,24 @@
 // generic-клас Library<T>
+interface Item{
+    getName(): string;
+}
 
-export class Library<T> {
+export class Library<T extends Item> {
+    private collection: T[] = [];
 
+    public add(item: T): void{
+        this.collection.push(item);
+    }
+
+    public getAll(): T[]{
+        return [...this.collection];
+    }
+
+    public remove(name: string): void {
+        this.collection = this.collection.filter(item => item.getName() !== name);
+    }
+
+    public getByName(name: string): T | undefined{
+        return this.collection.find(item => item.getName() === name);
+    }
 }
