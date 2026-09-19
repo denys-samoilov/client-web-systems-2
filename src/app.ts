43241c69a4667cbaf64feb4bf56199abba2485e6
Denys Samoilov <133922338+denys-samoilov@users.noreply.github.com>
2026-09-19 20:43:54 +0300

    feat: add form for user adding
    - Added form which has fields for the name of the user, and email

--- src/app.ts
+++ src/app.ts
@@ -59,8 +59,46 @@ class App {
         `;
      }
 
+     private renderAddUserElement(): string {
+        return `
+            <div class="container mt-5">
+                <div class="row justify-content-center">
+                    <div class="col-10">
+                        
+                        <div class="card p-4 shadow-sm border-0 rounded bg-white">
+                            <div class="card-body">
+                                
+                                <h2 class="card-title fw-bold text-dark mb-4">Додати користувача</h2>
+                                
+                                <form id="add-user-form">
+                                    
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control py-2 text-muted" id="user-name" placeholder="Ім'я">
+                                    </div>
+                                    
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control py-2 text-muted" id="user-email" placeholder="Email">
+                                    </div>
+                                    
+                                    <button type="submit" class="btn btn-success px-4 py-2">
+                                        Додати Користувача
+                                    </button>
+                                    
+                                </form>
+                                
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+
+    
+        `;
+    }
+
     private renderPage(): string{
-        return this.renderHeader() + this.renderAddBookElement();
+        return this.renderHeader() + this.renderAddBookElement() + this.renderAddUserElement();
     }
 }
 
