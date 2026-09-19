d1eb4a2b31283fadf6424eb8fad3778e8a6782a1
Denys Samoilov <133922338+denys-samoilov@users.noreply.github.com>
2026-09-19 21:16:06 +0300

    feat: add form with user
    - Added form which contains user info
    - User info contains id, name, and email

--- src/app.ts
+++ src/app.ts
@@ -134,8 +134,37 @@ class App {
     `;
     }
 
+    private renderUserList(): string{
+        return `
+        <div class="container mt-4">
+            <div class="row justify-content-center">
+                <div class="col-10">
+                    
+                    <div class="card shadow-sm border-0 rounded bg-white">
+                        <div class="card-body p-4">
+                            
+                            <h2 class="card-title fw-bold text-dark mb-4">Список Користувачів</h2>
+                            
+                            <div class="list-group list-group-flush"> 
+                                
+                                <div class="list-group-user d-flex justify-content-between align-items-center py-3 bg-transparent px-0 border-bottom">
+                                    <div class="text-dark">
+                                        12345678 William (willredd@gmail.com)
+                                    </div>
+                                </div>   
+                                
+                            </div>                                                        
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    `;
+    }
+
     private renderPage(): string{
-        return this.renderHeader() + this.renderAddBookElement() + this.renderAddUserElement() + this.renderBookList();
+        return this.renderHeader() + this.renderAddBookElement() + this.renderAddUserElement() + this.renderBookList() + this.renderUserList();
     }
 }
 
