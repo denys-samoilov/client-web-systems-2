7a0cb986b74ce7f6c38811d88cb78b7b2c581391
Denys Samoilov <133922338+denys-samoilov@users.noreply.github.com>
2026-09-19 20:36:43 +0300

    feat: add form for book adding
    - Added form which has fields for the name of the book, its author, and year of publishing

--- src/app.ts
+++ src/app.ts
@@ -19,8 +19,48 @@ class App {
         return '<h1 class = "text-center mt-5 fw-bold">Система управління бібліотекою</h1>'
     }
 
+     private renderAddBookElement(): string {
+        return `
+            <div class="container mt-5">
+                <div class="row justify-content-center">
+                    <div class="col-10">
+                        
+                        <div class="card p-4 shadow-sm border-0 rounded bg-white">
+                            <div class="card-body">
+                                
+                                <h2 class="card-title fw-bold text-dark mb-4">Додати Книгу</h2>
+                                
+                                <form id="add-book-form">
+                                    
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control py-2 text-muted" id="book-name" placeholder="Назва книги">
+                                    </div>
+                                    
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control py-2 text-muted" id="book-author" placeholder="Автор">
+                                    </div>
+                                    
+                                    <div class="mb-3">
+                                        <input type="text" class="form-control py-2 text-muted" id="book-year" placeholder="Рік видання">
+                                    </div>
+                                    
+                                    <button type="submit" class="btn btn-success px-4 py-2">
+                                        Додати Книгу
+                                    </button>
+                                    
+                                </form>
+                                
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        `;
+     }
+
     private renderPage(): string{
-        return this.renderHeader();
+        return this.renderHeader() + this.renderAddBookElement();
     }
 }
 
