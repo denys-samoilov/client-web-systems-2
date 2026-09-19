ec20ef08af0a5be9842363319f85f35dedab1559
Denys Samoilov <133922338+denys-samoilov@users.noreply.github.com>
2026-09-19 19:57:41 +0300

    build: implement and connect bootstrap
    - Implemented Bootstrap library to project

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,9 @@
 // точка входу, збирає все разом
 
+// import all modules here
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
 // etc.
 
 class App {
